6e9a707b835a9f2c01927419e4e4667fb7ad0bb2
lundjack123 <lundjack123@instance-2>
2020-09-08 21:55:45 +0000

    comment from python script

--- git_push.py
+++ git_push.py
@@ -1,6 +1,7 @@
 from git import Repo
 
-PATH_OF_GIT_REPO = r'C:\Users\lundj\AppData\Roaming\JetBrains\PyCharmCE2020.1\scratches\.git'  # make sure .git folder is properly configured
+#PATH_OF_GIT_REPO = r'C:\Users\lundj\AppData\Roaming\JetBrains\PyCharmCE2020.1\scratches\.git'  # make sure .git folder is properly configured
+PATH_OF_GIT_REPO = r'.git'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = 'comment from python script'
 
 def git_push():
